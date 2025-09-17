4dc1fe2e3a1a07e5198cde4120a8b686b1fc6474
Curve <fynnbwdt@gmail.com>
2025-09-18 01:07:19 +0200

    feat: add sponsor link [skip ci]

--- astro.config.ts
+++ astro.config.ts
@@ -12,9 +12,10 @@ export default defineConfig({
             title: "Saucer",
             description: "A modern, cross-platform C++ webview library",
             social: [
-                { icon: "github", label: "GitHub", href: "https://github.com/saucer/saucer" },
-                { icon: "codeberg", label: "CodeBerg", href: "https://codeberg.org/saucer/saucer" },
                 { icon: "discord", label: "Discord", href: "https://discord.gg/ndhmQE4225" },
+                { icon: "codeberg", label: "CodeBerg", href: "https://codeberg.org/saucer/saucer" },
+                { icon: "github", label: "GitHub", href: "https://github.com/saucer/saucer" },
+                { icon: "heart", label: "Sponsor", href: "https://github.com/sponsors/Curve" },
             ],
             logo: {
                 src: "./src/assets/logo.svg",
